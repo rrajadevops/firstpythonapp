2abc74c968267398e9438cd444374c22775b44c9
raja <raja@ge.com>
2018-02-06 23:02:52 +0530

    added new detailed view for personal with regular expersion

--- personal/urls.py
+++ personal/urls.py
@@ -1,6 +1,10 @@
-from django.urls import path
+from django.urls import path, re_path
 from . import views
 
 urlpatterns = [
+    #/personal/
     path('', views.index, name='index'),
+
+    #/personal/777/
+    re_path('^(?P<playercount>[0-9]+)/$', views.details, name='details'),
 ]
--- personal/views.py
+++ personal/views.py
@@ -1,3 +1,6 @@
 from django.http import HttpResponse
 def index(request):
     return HttpResponse("<h2>Name: <i>Rajasekaran Radhakrishnan</i></h2>")
+
+def details(request, playercount):
+    return HttpResponse("<h2>Details for playercount:" + str(playercount) + "</h2>")
