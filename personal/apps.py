c4297d7ad5064d6a6721cf3a0a74c54262a01eaa
raja <raja@ge.com>
2018-02-06 22:07:26 +0530

    After added db models, makemigrate, sqlmigrate and migrate

--- personal/apps.py
+++ personal/apps.py
@@ -1,5 +1,4 @@
 from django.apps import AppConfig
 
-
 class PersonalConfig(AppConfig):
     name = 'personal'
--- personal/models.py
+++ personal/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class hobby(models.Model):
+    name = models.CharField(max_length=100)
+    playercount = models.CharField(max_length=10)
+
+class social(models.Model):
+    activity = models.CharField(max_length=100)
+    users = models.CharField(max_length=100)
