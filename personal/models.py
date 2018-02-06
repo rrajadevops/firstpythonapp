30bf5bebcf1c4ece1932f28ea55e09c69880fb62
raja <raja@ge.com>
2018-02-06 22:24:59 +0530

    Will get tables values in python shell

--- personal/models.py
+++ personal/models.py
@@ -1,10 +1,15 @@
 from django.db import models
 
+
 # Create your models here.
 
 class hobby(models.Model):
     name = models.CharField(max_length=100)
     playercount = models.CharField(max_length=10)
+
+    def __str__(self):
+        return self.name + '-' + self.playercount
+
 
 class social(models.Model):
     activity = models.CharField(max_length=100)
