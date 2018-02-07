62eb5e46010b7d050536d48c85c7e74beb74c0f9
raja <raja@ge.com>
2018-02-07 23:34:05 +0530

    Added 404 exception

--- personal/views.py
+++ personal/views.py
@@ -1,16 +1,22 @@
-from django.http import HttpResponse
-from django.template import loader
+#from django.http import HttpResponse
+from django.http import Http404
+#from django.template import loader
 from django.shortcuts import render
 from .models import hobby, Album
+from django.core.exceptions import ObjectDoesNotExist
 
 
+all_album = Album.objects.all()
+context = {'all_album': all_album}
 def index(request):
-    all_album = Album.objects.all()
     #template = loader.get_template('personal/index.html')
-    context = {'all_album': all_album}
     #return HttpResponse(template.render(context, request))
-    return render(request, 'personal/index.html', context)
+    return render(request, 'personal/index.html', {'all_album': all_album})
 
 
 def details(request, playercount):
-    return HttpResponse("<h2>Details for playercount:" + str(playercount) + "</h2>")
+    try:
+        album = Album.objects.get(pk=playercount)
+    except ObjectDoesNotExist:
+        raise Http404("Album does not exist")
+    return render(request, 'personal/details.html', {'album': album})
