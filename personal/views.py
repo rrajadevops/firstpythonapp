fc1dc6d7f05a875667320960705f8eb1d747f2da
raja <raja@ge.com>
2018-02-07 23:05:36 +0530

    used render instead of template to view data

--- personal/views.py
+++ personal/views.py
@@ -1,15 +1,15 @@
 from django.http import HttpResponse
 from django.template import loader
+from django.shortcuts import render
 from .models import hobby, Album
 
 
 def index(request):
     all_album = Album.objects.all()
-    template = loader.get_template('personal/index.html')
-    context = {
-        'all_album': all_album,
-    }
-    return HttpResponse(template.render(context, request))
+    #template = loader.get_template('personal/index.html')
+    context = {'all_album': all_album}
+    #return HttpResponse(template.render(context, request))
+    return render(request, 'personal/index.html', context)
 
 
 def details(request, playercount):
